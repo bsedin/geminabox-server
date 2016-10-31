9fb80f1b6baeec078dedf3c242ac4e4ed5a9bb78
Lior Goikhburg <goikhburg@gmail.com>
2016-10-31 15:30:49 +0300

    optimizations to Docker file and fix for default variables

--- docker/run.sh
+++ docker/run.sh
@@ -2,10 +2,10 @@
 
 rm -rf /geminabox.pid /geminabox.sock
 
-export GEMINABOX_DATA=/data
 export GEMINABOX_PID=/geminabox.pid
-export GEMINABOX_HOST=0.0.0.0
-export GEMINABOX_PORT=8080
-export GEMINABOX_LOG=/geminabox.log
+export GEMINABOX_DATA=${GEMINABOX_DATA:-/data}
+export GEMINABOX_HOST=${GEMINABOX_HOST:-0.0.0.0}
+export GEMINABOX_PORT=${GEMINABOX_PORT:-8080}
+export GEMINABOX_LOG=${GEMINABOX_LOG:-/geminabox.log}
 
 bundle exec foreman start

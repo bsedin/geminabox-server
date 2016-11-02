ff7abd034468d56869ccbd3656812f2bdd4d9cae
Lior Goikhburg <goikhburg@gmail.com>
2016-11-02 17:09:12 +0300

    add GEMINABOX_RUBY_GEMS_URL environment variable

--- docker/run.sh
+++ docker/run.sh
@@ -7,5 +7,6 @@ export GEMINABOX_DATA=${GEMINABOX_DATA:-/data}
 export GEMINABOX_HOST=${GEMINABOX_HOST:-0.0.0.0}
 export GEMINABOX_PORT=${GEMINABOX_PORT:-8080}
 export GEMINABOX_LOG=${GEMINABOX_LOG:-/geminabox.log}
+export GEMINABOX_RUBY_GEMS_URL=${GEMINABOX_RUBY_GEMS_URL:-https://rubygems.org/}
 
 bundle exec foreman start
